6b51acc5356af38988a2b106aa8173ccd81895ae
pfreemanc <pfreemanc@gmail.com>
2020-11-10 13:09:48 -0500

    hooks, general class based structure/functional components/

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
-function App() {
+const App = (props) => {
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name: "Park Jimin", age: 25 },
+      { name: "Min Yoongi", age: 28 },
+      { name: "Hwasa", age: 25 },
+    ],
+  });
+
+  const [otherState, setOtherState] = useState('twit');
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        { name: "Paige", age: 25 },
+        { name: "Caity", age: 29 },
+        { name: "Hwasa", age: 25 },
+      ],
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hey</h1>
+      <button onClick={switchNameHandler}>switch name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
     </div>
   );
-}
+};
 
 export default App;
+
+// constructor() {
+//     super();
+//     this.state = {
+//   persons: [
+//     { name: 'Park Jimin', age: 25 },
+//     { name: 'Min Yoongi', age: 28 },
+//     { name: 'Hwasa', age: 25 },
+//   ],
+//   otherState: 'some other thing'
+// }
+//   }
+
+//   switchNameHandler = () => {
+//     // console.log('clicked');
+    // this.setState({
+    //   persons: [
+    //     { name: "Paige", age: 25 },
+    //     { name: "Caity", age: 29 },
+    //     { name: "Hwasa", age: 25 },
+    //   ],
+    // });
+//   }
